6dcf03b0042c2517d31130c3e59698ed776554a3
Subham kr Gupta <subham7031979@gmail.com>
2022-05-10 22:05:04 +0530

    Day 3 Class

---
+++ day3_geekSter.java
@@ -0,0 +1,75 @@
+import java.util.Scanner;
+
+public class day3_geekSter
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Any number : ");
+        int num = sc.nextInt();
+//
+//        if (num%2==0)
+//            System.out.println(num + " Num is a even Number...");
+//        else
+//            System.out.println(num + " Num is a odd Number...");
+//
+//        String result = (num%2==0) ? (num + " Number is a even number...") : (num + " Number is a Odd Number...");
+//        System.out.println(result);
+//
+//        for (int i = 1; i<=num; i++)
+//            System.out.println(i);
+//        for (int i = num; i>=1;i--)
+//            System.out.println(i);
+//
+//
+//        for (char i='a'; i<='z';i++)
+//            System.out.println(i);
+//        int count = 0;
+//        for(int i = 1; i<=num; i++) {
+//            if (i % 3 == 0) {
+//                System.out.println(i);
+//                count++;
+//            }
+//        }
+//        System.out.println("there are line " + count);
+//        int i=2;
+//        while(i<=num)
+//        {
+//            if (num%i==0)
+//                break;
+//            i++;
+//        }
+//        if (i==num)
+//            System.out.println(num + " is a prime number");
+//        else
+//            System.out.println(num + " is not a prime number");
+//
+//        for (i=2; i<=num;i++)
+//            if (num%i==0)
+//                break;
+//
+//        if (i == num)
+//            System.out.println(num + " is a prime number");
+//        else
+//            System.out.println(num + " is not a prime number");
+//
+//
+//        }
+//        for(int i = 1; i<=num; i++)
+//        {
+//            System.out.println("i*num = " + (3*i));
+//        }
+//
+//        for(int i =1;i<=num;i++)
+//            for (int j=1;j<=3;j++)
+//                System.out.println(i);
+//
+//        for (int i = 1; i<=num;i++)
+//            System.out.print("* ");
+//        for (int i = 1; i<=num;i++) {
+//            for (int j = 1; j <= num; j++)
+//                System.out.print("* ");
+//            System.out.println();
+//        }
+    }
+}
