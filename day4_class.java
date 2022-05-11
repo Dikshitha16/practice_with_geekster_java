e5dcdf15365324c096dda8b6acaa0140af0a8539
Subham kr Gupta <subham7031979@gmail.com>
2022-05-11 21:59:16 +0530

    Day 4 Class

---
+++ day4_class.java
@@ -0,0 +1,126 @@
+//package geekster;
+
+public class day4_class
+{
+//    int add(int a, int b)
+//    {
+//        return a+b;
+//    }
+    public static void main(String[] args)
+    {
+//        day4_class obj = new day4_class();
+//
+//        int a=2,b=10;
+//        int a1 = obj.add(a,b);
+//        System.out.println(a1);
+//        for (int i = 1; i<=5;i++)
+//        {
+//            for (int j = 1; j<=i; j++)
+//                System.out.print("* ");
+//            System.out.println();
+//        }
+//        int n =5;
+//        int n1 = n;
+//        for (int i = 1; i<=n;i++)
+//        {
+//            for (int j = n1; j>=1; j--)
+//            {
+//                System.out.print("* ");
+//            }
+//            n1--;
+//            System.out.println();
+//        }
+
+//            for (int i = 1; i<=n;i++)
+//            {
+//                for (int j = n1; j>=1;j--)
+//                {
+//                    System.out.print(" ");
+//                }
+//                n1--;
+//                for (int k = 1; k<=i;k++)
+//                    System.out.print(" *");
+//                System.out.println();
+//            }
+
+//        for (int i = 1; i<=n;i++)
+//        {
+//            for (int j = n1; j>=1; j--)
+//            {
+//                System.out.print(i);
+//            }
+//            n1--;
+//            System.out.println();
+//        }
+
+//        for (int i = 1; i<=5;i++)
+//        {
+//            for (int j = 1; j<=i; j++)
+//                System.out.print(j);
+//            System.out.println();
+//        }
+
+//        for (int i = 1; i<=n;i++)
+//        {
+//            for (int j = n1; j>=1;j--)
+//            {
+//                System.out.print(" ");
+//            }
+//            n1--;
+//            for (int k = 1; k<=i;k++)
+//                System.out.print("*");
+//            System.out.println();
+//        }
+//        int copy = 0;
+//        for (int i = 1; i<=n;i++)
+//        {
+//            for (int j = n1; j>=1;j--)
+//            {
+//                System.out.print(" ");
+//            }
+//            n1--;
+//            copy = i;
+//            if (i!=1)
+//            {
+//                copy = i + 1;
+//            }
+//            for (int k = 1; k<=i;k++)
+//                System.out.print("* ");
+//            System.out.println();
+//        for (int i = 0; i<n; i++)
+//        {
+//            for (int j =0; j<n-i-1; j++)
+//            {
+//                System.out.print("  ");
+//            }
+//            for (int j = 0; j<i+1;j++)
+//                System.out.print("* ");
+//            System.out.println();
+//        }
+
+//        for (int i = 0; i<n;i++)
+//        {
+//            for (int j = 97; j<=97+i;j++ )
+//            {
+//                System.out.print((char)j + " ");
+//            }
+//            System.out.println();
+//        }
+
+//        for (int i = 0; i<n;i++)
+//        {
+//            for (int j = 'a'; j<='a'+i;j++ )
+//            {
+//                System.out.print((char)j + " ");
+//            }
+//            System.out.println();
+//        }
+//        for (int i = 1; i<=5;i++)
+//        {
+//            for (int j = 0; j<i; j++)
+//                System.out.print((char)(j+97)+" ");
+//            System.out.println();
+//        }
+
+    }
+}
